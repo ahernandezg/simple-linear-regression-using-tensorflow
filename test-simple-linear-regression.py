e0270154fd7d374db9f1d2078ba7e7fa8c876a9f
a.hernandez.gtz@gmail.com <a.hernandez.gtz@gmail.com>
2024-08-24 09:06:52 +1000

    Update test-simple-linear-regression.py
    Created the plot_sin() function

--- test-simple-linear-regression.py
+++ test-simple-linear-regression.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def plot_sin(x,y):
+    plt.plot(x, y)
+    
 x = np.arange(0, 2*np.pi, 0.001)
 y = np.sin(x)
 
-
-plt.plot(x, y)
+plot_sin(x,y)
 plt.show()
+
