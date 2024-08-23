0fe68e2df20908b5cf7df50e852881a80c3dddae
a.hernandez.gtz@gmail.com <a.hernandez.gtz@gmail.com>
2024-08-24 09:12:24 +1000

    Update test-simple-linear-regression.py
    Deslkasjdalkjdalksjdlkajdlksa

--- test-simple-linear-regression.py
+++ test-simple-linear-regression.py
@@ -1,12 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_sin(x,y):
+def plot_sin(x):
+    y = np.sin(x)
     plt.plot(x, y)
-    
-x = np.arange(0, 2*np.pi, 0.001)
-y = np.sin(x)
 
-plot_sin(x,y)
+def plot_cos(x):
+    y = np.cos(x)
+    plt.plot(x,y)
+
+x = np.arange(0, 2*np.pi, 0.001)
+
+plot_sin(x)
+plot_cos(x)
+
+
 plt.show()
 
